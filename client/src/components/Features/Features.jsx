90a13cf91bf76c10ca91c7c1430cd8b28ee1a31e
Omkar Harade <omkarharade1301@gmail.com>
2023-08-11 14:06:45 +0530

    added features section code

--- client/src/components/Features/Features.jsx
+++ client/src/components/Features/Features.jsx
@@ -1,36 +1,39 @@
 import React, { Component } from 'react'
-import FeatureCard from './FeatureCard';
 import "./Features.css";
 
+import { faStar, faTruck, faCheckToSlot, faLight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
 export class Features extends Component {
-  render() {
-    return (
-      <>
-            <div className="features-div">
+    render() {
+        return (
+            <>
+                <div className="features-div">
 
-                <FeatureCard 
-                    customFeatureIcon = "fa-light fa-check-to-slot fa-beat"
-                    generalFeatureIcon = "general-icon"
-                    featureTitle="Verified Products" 
-                    featureDesc="Products are checked and sold by verified sellers"
+                    
+                    <div className="feature-card ">
+                        <FontAwesomeIcon className="general-feature-icon" icon={faCheckToSlot} iconStyle="light" size='6x' />
+                        <h3>Verified Products</h3>
+                        <p className='tertiary-text-p'>Products are checked and sold by verified sellers</p>
+                    </div>
 
-                />
-                
-                <FeatureCard 
-                    customFeatureIcon = "fa-light fa-truck"
-                    generalFeatureIcon = "general-icon"
-                    featureTitle="Fast Delivery" 
-                    featureDesc="Products delivered at your doorstep safely and quickly"/>
-                <FeatureCard 
-                    customFeatureIcon = "fa-light fa-check-to-slot fa-beat"
-                    generalFeatureIcon = "general-icon"
-                    featureTitle="Excellent Ratings" 
-                    featureDesc="Our success lies in happy and satisfied customers"/>
-            </div>
+                    <div className="feature-card ">
+                        <FontAwesomeIcon className="general-feature-icon" icon={faTruck} iconStyle="light" size='6x'/>
+                        <h3>Fast Delivery</h3>
+                        <p className='tertiary-text-p'>Products delivered at your doorstep safely and quickly</p>
+                    </div>
 
-      </>
-    )
-  }
+                    <div className="feature-card ">
+                        <FontAwesomeIcon className="general-feature-icon" icon={faStar} iconStyle="light"size='6x'/>
+                        <h3>Excellent Ratings</h3>
+                        <p className='tertiary-text-p'>Our success lies in happy and satisfied customers</p>
+                    </div>
+                </div>
+
+            </>
+        )
+    }
 }
 
 export default Features
