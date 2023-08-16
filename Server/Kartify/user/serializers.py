d63aa2f03617e0fa740f8c42a9cfcb305ae55888
palak158 <palakagrawal158@gmail.com>
2023-08-16 19:26:15 +0100

    Udated

--- Server/Kartify/user/serializers.py
+++ Server/Kartify/user/serializers.py
@@ -7,6 +7,14 @@ class BuyerSerializer(serializers.ModelSerializer):
     class Meta :
         model = Buyers
         fields = "__all__"
+        extra_kwargs = {
+               'password' : {'write_only': True}
+                        }
+        
+    # def create(self,validated_data):
+    #        password = validated_data.pop('password', None)
+
+    
         
 
 class LoginSerializer(serializers.Serializer):
